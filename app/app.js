eeefc84248bac785b9a46175e57b40693cfdabfb
ivayloilievv <ivotranseood@abv.bg>
2016-04-14 13:13:55 +0300

    urlEx

--- app/app.js
+++ app/app.js
@@ -10,4 +10,4 @@ angular.module('socialNetwork', [
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/'});
   }])
-  .constant('BASE_URL', 'http://softuni-social-network.azurewebsites.net/api/');
+  .constant('BASE_URL', 'http://softuni-issue-tracker.azurewebsites.net/api/');
