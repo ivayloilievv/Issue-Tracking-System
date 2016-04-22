7b7840f81529a7e87c4321dc0ce038f515dd0eed
ivayloilievv <ivotranseood@abv.bg>
2016-04-22 10:50:46 +0300

    projects

--- app/home/home-controller.js
+++ app/home/home-controller.js
@@ -34,4 +34,39 @@ angular.module('issueTrackingSystem.home', [
                         })
                 })
         };
+
+            function GetCurrentUser() {
+                var deferred = $q.defer();
+
+                $http.get(BASE_URL + 'Users/me',
+                    { headers: {'Authorization': sessionStorage['TokenType'] + " " + sessionStorage['AccessToken']}})
+                    .then(function (result) {
+                        deferred.resolve(result.data);
+                    },function (err) {
+                        deferred.reject(err);
+                    })
+                return deferred.promise;
+            }
+
+            function GetAllUsers() {
+                var deferred = $q.defer();
+
+                $http.get(BASE_URL + 'Users',
+                    { headers: {'Authorization': sessionStorage['TokenType'] + " " + sessionStorage['AccessToken']}})
+                    .then(function (result) {
+                        deferred.resolve(result.data);
+                    },function (err) {
+                        deferred.reject(err);
+                    })
+                return deferred.promise;
+            }
+
+
+
+            return {
+                Register: Register,
+                Login: Login,
+                GetCurrentUser: GetCurrentUser,
+                GetAllUsers: GetAllUsers
+            }
     }])
