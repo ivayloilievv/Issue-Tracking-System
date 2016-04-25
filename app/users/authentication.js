4181b4ee7e6e804f94f99f608d68066277d1115b
ivayloilievv <ivotranseood@abv.bg>
2016-04-25 17:14:29 +0300

    auth

--- app/users/authentication.js
+++ app/users/authentication.js
@@ -37,6 +37,48 @@ angular.module('issueTrackingSystem.users.authentication', [])
                 return deferred.promise;
             }
 
+            function GetCurrentUser() {
+                var deferred = $q.defer();
+
+                $http.get(BASE_URL + 'Users/me',
+                    { headers: {'Authorization': sessionStorage['TokenType'] + " " + sessionStorage['AccessToken']}})
+                    .then(function (result) {
+                        deferred.resolve(result.data);
+                    },function (err) {
+                        deferred.reject(err);
+                    })
+                return deferred.promise;
+            }
+
+            function GetAllUsers() {
+                var deferred = $q.defer();
+
+                $http.get(BASE_URL + 'Users',
+                    { headers: {'Authorization': sessionStorage['TokenType'] + " " + sessionStorage['AccessToken']}})
+                    .then(function (result) {
+                        deferred.resolve(result.data);
+                    },function (err) {
+                        deferred.reject(err);
+                    })
+                return deferred.promise;
+            }
+
+            function ChangePassword(User) {
+
+                var deferred = $q.defer();
+
+                $http.post(BASE_URL + 'api/Account/ChangePassword', User ,
+                    { headers: {'Authorization': sessionStorage['TokenType'] + " " + sessionStorage['AccessToken']}})
+                    .then(function (result) {
+                        deferred.resolve(result.data);
+                    },function (err) {
+                        deferred.reject(err);
+                    })
+
+                return deferred.promise;
+            }
+
+
             function  logout() {
 
             }
@@ -44,6 +86,9 @@ angular.module('issueTrackingSystem.users.authentication', [])
             return {
                 registerUser: registerUser,
                 loginUser: loginUser,
+                GetCurrentUser: GetCurrentUser,
+                GetAllUsers: GetAllUsers,
+                ChangePassword: ChangePassword,
                 logout: logout
             }
     }])
