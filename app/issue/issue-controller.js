300b4c98e24409a30ab2734bb51f66882b1664f3
ivayloilievv <ivotranseood@abv.bg>
2016-05-07 13:22:19 +0300

    changeStatus

--- app/issue/issue-controller.js
+++ app/issue/issue-controller.js
@@ -24,6 +24,7 @@ angular.module('issueTrackingSystem.issue-controller', [
             IssueServices.GetIssueById($routeParams.id)
                 .then(function (success) {
                     $scope.Issue = success;
+                    console.log($scope.Issue);
                 });
 
             IssueServices.GetCommentsByIssueId($routeParams.id)
@@ -37,6 +38,17 @@ angular.module('issueTrackingSystem.issue-controller', [
                 .then(function (success) {
                     $scope.CurrentUserId = success.Id;
                 });
+
+            $scope.ChangeStatus = function (StatusId) {
+                IssueServices.ChangeStatus($routeParams.id, StatusId)
+            }
+
+            $scope.AddComment = function (Comment) {
+                var obj = {
+                    Text: Comment
+                }
+                IssueServices.AddComment($routeParams.id, obj);
+            }
 
             $scope.loopData = function (Data) {
                 var result = "";
@@ -57,88 +69,3 @@ angular.module('issueTrackingSystem.issue-controller', [
         }]
     );
 
-angular.module('issueTrackingSystem.controllers.addIssue', [
-        'issueTrackingSystem.services.issue',
-        'issueTrackingSystem.services.auth',
-        'issueTrackingSystem.services.project'
-    ])
-    .config(['$routeProvider', function($routeProvider){
-        $routeProvider.when('/project/:id/add-issue', {
-            templateUrl: 'app/templates/addIssue.html',
-            controller: 'AddIssueController'
-        })
-    }])
-    .controller('AddIssueController', [
-        '$scope',
-        '$location',
-        '$routeParams',
-        '$window',
-        'IssueServices',
-        'AuthServices',
-        'ProjectServices',
-        function AddIssueController($scope, $location, $routeParams, $window, IssueServices, AuthServices, ProjectServices) {
-            $scope.Issue = {};
-            ProjectServices.GetAllProjects()
-                .then(function (success) {
-                    $scope.Projects = success;
-                });
-
-            ProjectServices.GetProjectById($routeParams.id)
-                .then(function (success) {
-                    $scope.Issue.Project = success;
-                    $scope.Issue.Labels = $scope.loopData($scope.Issue.Project.Labels);
-                });
-
-            AuthServices.GetCurrentUser()
-                .then(function (success) {
-                    $scope.Issue.Assignee = success;
-
-                });
-
-            AuthServices.GetAllUsers()
-                .then(function (success) {
-                    $scope.Users = success;
-                });
-
-            AuthServices.GetCurrentUser()
-                .then(function (success) {
-                    $scope.CurrentUserId = success.Id;
-                });
-
-            $scope.loopData = function (Data) {
-                var result = "";
-                if(Data != undefined){
-                    Data.forEach(function(element) {
-                        result+= element.Name + ', ';
-                    }, this);
-                }
-                result = result.substr(0, result.length-2);
-                return result;
-            }
-
-            $scope.Add = function (Issue) {
-                var labels = "";
-                labels = Issue.Labels;
-                labels = labels.split(", ");
-
-                var newLabels= [];
-                for (var index = 0; index < labels.length; index++) {
-                    newLabels[index] = { Name: labels[index]};
-                }
-
-                var IssueForRequest = {
-                    Title: Issue.Title,
-                    Description: Issue.Description,
-                    DueDate: Issue.DueDate,
-                    ProjectId: Issue.Project.Id,
-                    AssigneeId: Issue.Assignee.Id,
-                    PriorityId: Issue.Priority.Id,
-                    Labels: newLabels
-                };
-                IssueServices.PostIssue(IssueForRequest)
-                    .then(function(success) {
-                        $location.path('/dashboard/1')
-                    });
-            }
-        }
-    ]);
